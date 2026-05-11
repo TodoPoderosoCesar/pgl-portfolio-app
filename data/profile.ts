46bf836cf5558912745494d83a4b160e34679265
cesarbritoo <guillegarciabrito@gmail.com>
2026-05-11 16:47:32 +0100

    refactor(data): extraction of the profile data

---
+++ data/profile.ts
@@ -0,0 +1,22 @@
+export const PROFILE = {
+  name: 'Mi nombre',
+  description:
+    'Soy profe y me gusta mi trabajo aunque a veces me dé por enrevesar prácticas para mis queridos alumnos.',
+  avatar: require('../assets/SofyanAmrabat.jpg'),
+  repoUrl: 'https://github.com/adhernea',
+};
+
+export const INTERESTS: string[] = [
+  'Salir a pasear',
+  'Senderismo',
+  'Ir a la playita',
+  'Domingos de misa',
+  'La guitarrita',
+  'El monte con lluvia',
+  'Viajar',
+  'Música variadita',
+  'Anime',
+  'Ducharme',
+  'Videojuegos',
+  'Ir de cenar romántica',
+];
